369a0ce0d93145091e172c93326c68f39a820ab5
guilhermedom <guilhermedfs@gmail.com>
2022-08-13 22:56:44 -0300

    Upload third version of CreateTable.sql script
    Update script to reflect changes in the database model:
    
    - Add more geographical columns to table customers;
    
    - Add sex column to table customers;
    
    - Add discount column to table orders;
    
    - Make more columns NOT NULL to avoid having to deal with undetermined null cases in code;
    
    - Reorganize column order to be more intuitive;
    
    - Add new columns to the relational database to save total price of order before and after discount;
    
    - Add discount percentage column;
    
    - Set email as unique in tables "customers" and "salespersons". Now it is an alternate key;
    
    - Set model_brand, model_name, cpu, memory and generation to be a composite unique constraint on table models. Now it is an alternate key;
    
    - Use more specific data types for INT columns (i.e., BIGINT, SMALLINT).

--- src/data/1.CreateTable.sql
+++ src/data/1.CreateTable.sql
@@ -5,50 +5,60 @@ CREATE SCHEMA relational;
 CREATE SEQUENCE relational.model_id;
 CREATE TABLE relational.models(
 	model_id INT DEFAULT nextval('relational.model_id'::regclass) PRIMARY KEY,
-	generation INT,
+	model_brand VARCHAR(200) NOT NULL,
+	model_name VARCHAR(200) NOT NULL,
 	operating_system VARCHAR(50) NOT NULL,
 	cpu VARCHAR(50) NOT NULL,
 	memory VARCHAR(50) NOT NULL,
-	service_category VARCHAR(50)
+	generation SMALLINT NOT NULL,
+	service_category VARCHAR(50) NOT NULL,
+	UNIQUE(model_brand, model_name, cpu, memory, generation)
 );
 
 CREATE SEQUENCE relational.robot_id;
 CREATE TABLE relational.robots(
-	robot_id INT DEFAULT nextval('relational.robot_id'::regclass) PRIMARY KEY,
+	robot_id BIGINT DEFAULT nextval('relational.robot_id'::regclass) PRIMARY KEY,
 	model_id INT NOT NULL REFERENCES relational.models(model_id),
-	acquisition_date DATE NOT NULL,
 	register_plate VARCHAR(50) NOT NULL UNIQUE,
-	daily_rate NUMERIC(10, 2) NOT NULL,
+	acquisition_date DATE NOT NULL,
+	daily_rate NUMERIC(8, 2) NOT NULL,
 	status VARCHAR(50) NOT NULL
 );
 
 CREATE SEQUENCE relational.customer_id;
 CREATE TABLE relational.customers(
-	customer_id INT DEFAULT nextval('relational.customer_id'::regclass) PRIMARY KEY,
-	document_number VARCHAR(50) NOT NULL UNIQUE,
+	customer_id BIGINT DEFAULT nextval('relational.customer_id'::regclass) PRIMARY KEY,
+	email VARCHAR(200) NOT NULL UNIQUE,
 	name VARCHAR(200) NOT NULL,
 	date_of_birth DATE NOT NULL,
+	sex VARCHAR(12) NOT NULL,
 	phone_number VARCHAR(20) NOT NULL,
-	address VARCHAR(200),
+	state_province VARCHAR(200) NOT NULL,
+	city VARCHAR(200) NOT NULL,
+	address VARCHAR(200) NOT NULL,
 	date_of_registration DATE NOT NULL,
 	status VARCHAR(50) NOT NULL
 );
 
-CREATE SEQUENCE relational.salesman_id;
-CREATE TABLE relational.salesmen(
-	salesman_id INT DEFAULT nextval('relational.salesman_id'::regclass) PRIMARY KEY,
+CREATE SEQUENCE relational.salesperson_id;
+CREATE TABLE relational.salespersons(
+	salesperson_id INT DEFAULT nextval('relational.salesperson_id'::regclass) PRIMARY KEY,
+	email VARCHAR(200) NOT NULL UNIQUE,
 	name VARCHAR(200) NOT NULL,
-	branch VARCHAR(50),
+	branch VARCHAR(50) NOT NULL,
 	status VARCHAR(50) NOT NULL
 );
 
 CREATE SEQUENCE relational.order_id;
 CREATE TABLE relational.orders(
-	order_id INT DEFAULT nextval('relational.order_id'::regclass) PRIMARY KEY,
-	robot_id INT NOT NULL REFERENCES relational.robots(robot_id),
-	customer_id INT NOT NULL REFERENCES relational.customers(customer_id),
-	salesman_id INT NOT NULL REFERENCES relational.salesmen(salesman_id),
+	order_id BIGINT DEFAULT nextval('relational.order_id'::regclass) PRIMARY KEY,
+	robot_id BIGINT NOT NULL REFERENCES relational.robots(robot_id),
+	customer_id BIGINT NOT NULL REFERENCES relational.customers(customer_id),
+	salesperson_id INT NOT NULL REFERENCES relational.salespersons(salesperson_id),
 	order_start_date DATE NOT NULL,
 	order_end_date DATE,
-	total_price NUMERIC(10, 2)
+	total_price NUMERIC(12, 2),
+	discount_percentage NUMERIC(3, 2),
+	discount NUMERIC(12, 2),
+	discounted_total_price NUMERIC(12, 2)
 );
